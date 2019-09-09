7e883377a9bfe835cf4ed45a5dc20d5563d3b6a9
Eccenux <eccenux+git@gmail.com>
2019-09-09 12:31:59 +0200

    Active Sessions auto-stack

--- lld_all_graph.pl
+++ lld_all_graph.pl
@@ -339,13 +339,13 @@ sub creategraph
 	# password
 	$password = "zabbix";
 	# only add graphs to hosts linked to this template
-	$template = "WIN Windows";
+	$template = "AutoTomcatGraphs";
 	# internal
 	$header = "Content-Type:application/json";
 	# intenal zabbix url
-	$url = "http://127.0.0.1/api_jsonrpc.php";
+	$url = "http://127.0.0.1:8081/zabbix/api_jsonrpc.php";
 	# create a graph with this name in each host
-	$graph = 'WIN Volume "ALL" bytes/sec stack';
+	$graph = 'Active Sessions auto-stack';
 	$graphtype = 1; ### 0=normal, 1=stacked
 	$mintype = 1; ### 0=calculated, 1=fixed
 	$maxtype = 0; ### 0=calculated, 1=fixed
@@ -355,9 +355,8 @@ sub creategraph
 	$drawtype = 0; ### 0=line, 1=filled, 2=boldline, 3=dot, 4=dashed, 5=gradient
 	$calcfunction = 2; ### 1=min, 4=max, 2=avg, 7=all
 	# add graph items mathing these regexes, maximum 2
-	$regexes[0] = '^WIN\sVolume\s".*"\sbytes/sec\swrite';
-	$regexes[1] = '^WIN\sVolume\s".*"\sbytes/sec\sread';
-
+	$regexes[0] = '^activeSessions\s.*';
+	#$regexes[1] = '^WIN\sVolume\s".*"\sbytes/sec\sread';
 
 
 ##########
