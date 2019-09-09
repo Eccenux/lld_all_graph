aae9ee459c1fd0b37f254dbcca8138e680ff3cb5
Eccenux <eccenux+git@gmail.com>
2019-09-09 12:52:47 +0200

    external configuration

--- lld_all_graph.pl
+++ lld_all_graph.pl
@@ -334,29 +334,7 @@ sub creategraph
 	#########
 	# modify these values accordingly
 	#########
-	# user
-	$user = "Admin";
-	# password
-	$password = "zabbix";
-	# only add graphs to hosts linked to this template
-	$template = "AutoTomcatGraphs";
-	# internal
-	$header = "Content-Type:application/json";
-	# intenal zabbix url
-	$url = "http://127.0.0.1:8081/zabbix/api_jsonrpc.php";
-	# create a graph with this name in each host
-	$graph = 'Active Sessions auto-stack';
-	$graphtype = 1; ### 0=normal, 1=stacked
-	$mintype = 1; ### 0=calculated, 1=fixed
-	$maxtype = 0; ### 0=calculated, 1=fixed
-	$minvalue = 0;
-	$maxvalue = 100;
-	$showtriggers = 1;
-	$drawtype = 0; ### 0=line, 1=filled, 2=boldline, 3=dot, 4=dashed, 5=gradient
-	$calcfunction = 2; ### 1=min, 4=max, 2=avg, 7=all
-	# add graph items mathing these regexes, maximum 2
-	$regexes[0] = '^activeSessions\s.*';
-	#$regexes[1] = '^WIN\sVolume\s".*"\sbytes/sec\sread';
+	require './config.pl';
 
 
 ##########
